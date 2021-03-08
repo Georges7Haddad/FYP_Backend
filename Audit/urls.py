73245ca3439f5479084b8e4053f2af098a15d6f6
Georges Haddad <georges7haddad@gmail.com>
2021-03-08 18:20:21 +0200

    [Add] Spleeter Integration [Add] APIs for the different stem tracks

--- Audit/urls.py
+++ Audit/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from Audit.views import Split
+from Audit.views import *
 
 urlpatterns = [
     path('split/', Split.as_view()),
+    path('vocals/', VocalsAudio.as_view()),
+    path('drums/', DrumsAudio.as_view()),
+    path('piano/', PianoAudio.as_view()),
+    path('bass/', BassAudio.as_view()),
+    path('other/', OtherAudio.as_view()),
 ]
--- Audit/views.py
+++ Audit/views.py
@@ -1,25 +1,63 @@
+import subprocess
+import os
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from wsgiref.util import FileWrapper
 
 
 class Split(APIView):
-    parser_classes = (MultiPartParser, )
+    parser_classes = (MultiPartParser,)
 
     @csrf_exempt
     def post(self, request):
-        # with open("Audit/new.mp3", "wb") as new_file:
-        #     new_file.write(request.FILES['file'].read())
-        vocals = open("Audit/rap.MP3", "rb")
-        vocals_response = HttpResponse(FileWrapper(vocals), content_type="audio/mpeg", status=status.HTTP_201_CREATED)
-        vocals_response['Content-Disposition'] = 'attachment; filename="rap.MP3"'
-        return vocals_response
+        with open("Audit/original_track.mp3", "wb") as new_file:
+            new_file.write(request.FILES['file'].read())
+        subprocess.run(["spleeter", "separate", "-p", "spleeter:5stems", "-o", "output", "Audit/original_track.mp3"])
+        os.remove("Audit/original_track.mp3")
+        return Response(status=status.HTTP_201_CREATED)
 
+
+class VocalsAudio(APIView):
     def get(self, request):
-        drums = open("Audit/Kzood.MP3", "rb")
+        drums = open("output/original_track/vocals.wav", "rb")
         drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
-        drums_response['Content-Disposition'] = 'attachment; filename="rap.MP3"'
+        drums_response['Content-Disposition'] = 'attachment; filename="vocals.MP3"'
+        return drums_response
+
+
+class DrumsAudio(APIView):
+    def get(self, request):
+        drums = open("output/original_track/drums.wav", "rb")
+        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        drums_response['Content-Disposition'] = 'attachment; filename="drums.MP3"'
+        return drums_response
+
+
+class PianoAudio(APIView):
+    def get(self, request):
+        drums = open("output/original_track/piano.wav", "rb")
+        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        drums_response['Content-Disposition'] = 'attachment; filename="piano.MP3"'
+        return drums_response
+
+
+class BassAudio(APIView):
+    def get(self, request):
+        drums = open("output/original_track/bass.wav", "rb")
+        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        drums_response['Content-Disposition'] = 'attachment; filename="piano.MP3"'
+        return drums_response
+
+
+class OtherAudio(APIView):
+    def get(self, request):
+        drums = open("output/original_track/other.wav", "rb")
+        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        drums_response['Content-Disposition'] = 'attachment; filename="others.MP3"'
+        subprocess.run(["rm", "-r", "output"])
         return drums_response
