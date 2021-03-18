01fe97dac5a9e8f9842799da6055d49f92c56d6a
Georges Haddad <georges7haddad@gmail.com>
2021-03-18 22:40:33 +0200

    [Fix] Support multiple splits [Fix] Get URIs

--- Audit/urls.py
+++ Audit/urls.py
@@ -4,9 +4,9 @@ from Audit.views import *
 
 urlpatterns = [
     path('split/', Split.as_view()),
-    path('vocals/', VocalsAudio.as_view()),
-    path('drums/', DrumsAudio.as_view()),
-    path('piano/', PianoAudio.as_view()),
-    path('bass/', BassAudio.as_view()),
-    path('other/', OtherAudio.as_view()),
+    path('vocals/<int:rand>/', VocalsAudio.as_view()),
+    path('drums/<int:rand>/', DrumsAudio.as_view()),
+    path('piano/<int:rand>/', PianoAudio.as_view()),
+    path('bass/<int:rand>/', BassAudio.as_view()),
+    path('other/<int:rand>/', OtherAudio.as_view()),
 ]
--- Audit/views.py
+++ Audit/views.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+from random import randrange
+from threading import Timer
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -10,54 +12,61 @@ from rest_framework.views import APIView
 from wsgiref.util import FileWrapper
 
 
+def delete_output(rand):
+    subprocess.run(["rm", "-r", f"output{rand}"])
+
+
 class Split(APIView):
     parser_classes = (MultiPartParser,)
 
     @csrf_exempt
     def post(self, request):
+        rand = randrange(1000)
         with open("Audit/original_track.mp3", "wb") as new_file:
             new_file.write(request.FILES['file'].read())
-        subprocess.run(["spleeter", "separate", "-p", "spleeter:5stems", "-o", "output", "Audit/original_track.mp3"])
+        subprocess.run(
+            ["spleeter", "separate", "-p", "spleeter:5stems", "-o", f"output{rand}", "Audit/original_track.mp3"])
         os.remove("Audit/original_track.mp3")
-        return Response(status=status.HTTP_201_CREATED)
+        t = Timer(300.0, delete_output, args=[rand])
+        t.start()
+        return Response({"rand": rand}, status=status.HTTP_201_CREATED)
 
 
 class VocalsAudio(APIView):
-    def get(self, request):
-        drums = open("output/original_track/vocals.wav", "rb")
-        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
-        drums_response['Content-Disposition'] = 'attachment; filename="vocals.MP3"'
-        return drums_response
+    def get(self, request, rand):
+        vocals = open(f"output{rand}/original_track/vocals.wav", "rb")
+        vocals_response = HttpResponse(FileWrapper(vocals), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        vocals_response['Content-Disposition'] = 'attachment; filename="vocals.MP3"'
+        return vocals_response
 
 
 class DrumsAudio(APIView):
-    def get(self, request):
-        drums = open("output/original_track/drums.wav", "rb")
+    def get(self, request, rand):
+        drums = open(f"output{rand}/original_track/drums.wav", "rb")
         drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
         drums_response['Content-Disposition'] = 'attachment; filename="drums.MP3"'
         return drums_response
 
 
 class PianoAudio(APIView):
-    def get(self, request):
-        drums = open("output/original_track/piano.wav", "rb")
-        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
-        drums_response['Content-Disposition'] = 'attachment; filename="piano.MP3"'
-        return drums_response
+    def get(self, request, rand):
+        piano = open(f"output{rand}/original_track/piano.wav", "rb")
+        piano_response = HttpResponse(FileWrapper(piano), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        piano_response['Content-Disposition'] = 'attachment; filename="piano.MP3"'
+        return piano_response
 
 
 class BassAudio(APIView):
-    def get(self, request):
-        drums = open("output/original_track/bass.wav", "rb")
-        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
-        drums_response['Content-Disposition'] = 'attachment; filename="piano.MP3"'
-        return drums_response
+    def get(self, request, rand):
+        bass = open(f"output{rand}/original_track/bass.wav", "rb")
+        bass_response = HttpResponse(FileWrapper(bass), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        bass_response['Content-Disposition'] = 'attachment; filename="piano.MP3"'
+        return bass_response
 
 
 class OtherAudio(APIView):
-    def get(self, request):
-        drums = open("output/original_track/other.wav", "rb")
-        drums_response = HttpResponse(FileWrapper(drums), content_type="audio/mpeg", status=status.HTTP_200_OK)
-        drums_response['Content-Disposition'] = 'attachment; filename="others.MP3"'
-        subprocess.run(["rm", "-r", "output"])
-        return drums_response
+    def get(self, request, rand):
+        other = open(f"output{rand}/original_track/other.wav", "rb")
+        other_response = HttpResponse(FileWrapper(other), content_type="audio/mpeg", status=status.HTTP_200_OK)
+        other_response['Content-Disposition'] = 'attachment; filename="others.MP3"'
+        return other_response
